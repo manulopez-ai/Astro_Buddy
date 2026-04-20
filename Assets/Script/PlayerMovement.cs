281d3d1d20e12c2a36786b3618a58373dfff72db
manulopez-ai <malopezr2@eafit.edu.co>
2026-04-19 20:26:37 -0500

    cristal
    se añadieron cristales, los cuales son esenciales para poder atacar al enemigo.
    se realizaron ajustes generales en el juego.
    se cambio el fondo.
    se implemento el estado Game Over.
    se elimino la mecanica de perder puntos al tocar al enemigo.
    se añadieron mas enemigos para aumentar la dificultad.
    se agregaron mas estrellas .

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using NUnit.Framework;
 using TMPro;
 using UnityEngine;
@@ -10,10 +11,12 @@ public class PlayerMovement : MonoBehaviour
     public GameObject Pistola;
     public int estrellasParaPistola= 5;
     public bool hasPistola= false;
-    public int damageEnemy = 1;
+    public bool isGameOver = false;
     public bool puedeAtacar = false;
     public bool hasWon = false;
     public int enemigosDerrotados = 0;
+    public int cristales=0;
+    public int estrellas =0;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -31,16 +34,19 @@ public class PlayerMovement : MonoBehaviour
         Vector3 direction = new Vector3 (moveHorizontal, moveVertical, 0);
         transform.Translate(direction * speed * Time.deltaTime);
 
-        if(score >= estrellasParaPistola && hasPistola)
+        if(estrellas >= estrellasParaPistola && hasPistola)
         {
             puedeAtacar = true;
         }
 
-        if(puedeAtacar && enemigosDerrotados >= 3 && ! hasWon)
+        if(puedeAtacar && enemigosDerrotados >= 4
+         && ! hasWon)
         {
             hasWon = true;
             Debug.Log("You Won!");
         }
+
+        if(isGameOver) return;
     }
 
 
@@ -48,18 +54,29 @@ public class PlayerMovement : MonoBehaviour
     {
        if(other.CompareTag("Star"))
         {
+            estrellas++;// suma 1 a la variable de estrella
             score = score + 1;
 
             Destroy(other.gameObject);
             Debug.Log("Star collected!");
             Debug.Log("Score:"+ score); 
 
-         if(score>=5)
+         if(estrellas>= estrellasParaPistola)
             {
                 Pistola.SetActive(true);
                 Debug.Log("Pistola desbloqueada");
             }
         } 
+        if(other.CompareTag("cristal"))
+        {
+          cristales++;
+          score+=5;
+          Destroy(other.gameObject);
+          Debug.Log("Cristal recogido. Total:"+ cristales);
+  
+        }
+
+
         if(other.CompareTag("Pistola"))
         {
             hasPistola = true;
@@ -67,22 +84,38 @@ public class PlayerMovement : MonoBehaviour
             Debug.Log("Pistola recogida");
 
         }
-        if(other.CompareTag("Enemy"))
-        {   if(puedeAtacar)
-            {   enemigosDerrotados = enemigosDerrotados + 1; // sumar 1 al contador de enemigos derrotados
+        if(other.CompareTag("Enemy") && !isGameOver)
+        {   if(puedeAtacar && cristales >0 )
+            {   
+                cristales--;// gasta un cristal
+                enemigosDerrotados = enemigosDerrotados + 1; // sumar 1 al contador de enemigos derrotados
                 Destroy(other.gameObject);
                 Debug.Log("Enemigo destruido");
             }
             else
             {
-                score = score - 1;
-                Debug.Log("Te golpeó un enemigo. score:"+ score); 
+                isGameOver =true;
+
+                Debug.Log("Te golpeó un enemigo, Game Over");
+
+                StartCoroutine(RestartLevel());
+
             }
         
            
 
 
         }
+        
        
+        }
+
+     IEnumerator RestartLevel()// permite esperar un tiempo para reiniciar
+        {
+            yield return new WaitForSeconds(2f);// espera 2 segundos
+
+            UnityEngine.SceneManagement.SceneManager.LoadScene(
+                UnityEngine.SceneManagement.SceneManager.GetActiveScene().name
+            );// vuelve a cargar la escena osea reinicia el juego    
     }
 }
